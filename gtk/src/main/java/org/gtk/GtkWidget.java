28e6426afa9788403f6a9a46b9587d19b4864f0a
Bret Joseph Antonio <bretantonio@users.noreply.github.com>
2021-11-14 16:40:09 +0200

    Update GtkWidget.java

--- gtk/src/main/java/org/gtk/GtkWidget.java
+++ gtk/src/main/java/org/gtk/GtkWidget.java
@@ -4,4 +4,12 @@ import android.view.View;
 
 public class GtkWidget {
     View view;
+    
+    public void set_vexpand(boolean expand){
+        if(view.getClass() == LinearLayout.class && expand){
+            LinearLayout.LayoutParams parameters = new 
+                LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        }
+        
+    }
 }
