b74787614c15cdb6e4801783dd13ca2eada1b3d1
bretantonio <bretjoseph@gmail.com>
2022-01-26 10:43:31 +0200

    Update

---
+++ src/main/java/org/gtk/GtkImage.kt
@@ -0,0 +1,9 @@
+package org.gtk
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+
+class GtkImage(context: Context, attributeSet: AttributeSet):
+    ImageView(context, attributeSet) {
+}
