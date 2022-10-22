bcb6ce13d497aa529abbe16f48f1b0861f03cb5f
Bret Joseph <bretjoseph@gmail.com>
2022-10-22 18:53:05 +0200

    Update

--- src/main/java/org/gtk/GtkNotebook.java
+++ src/main/java/org/gtk/GtkNotebook.java
@@ -1,8 +1,11 @@
 package org.gtk;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
@@ -10,60 +13,71 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
-import android.widget.Space;
 
 public class GtkNotebook extends LinearLayout implements GtkWidget {
     private final LinearLayout pageTitles;
     private final FrameLayout pageFrame;
+    private final LinearLayout space, barColour;
     private int currentPage = 0, previousPage = 0;
     private float startX = 0f;
 
+    @SuppressLint("ClickableViewAccessibility")
     public GtkNotebook(Context context, AttributeSet attributes) {
         super(context, attributes);
         HorizontalScrollView scrollView = new HorizontalScrollView(context);
+        barColour = new LinearLayout(context);
         pageTitles = new LinearLayout(context);
         pageFrame = new FrameLayout(context);
-        setOrientation(LinearLayout.VERTICAL);
+        space = new LinearLayout(context);
+        setOrientation(VERTICAL);
+        pageTitles.setGravity(Gravity.START);
         addView(scrollView, new LayoutParams(-1, -2));
         addView(pageFrame, new LayoutParams(-1, -1));
         scrollView.addView(pageTitles, new LayoutParams(-1, -2));
         scrollView.setFillViewport(true);
+        pageTitles.addView(space, new LayoutParams(-1, 88));
+        space.setPadding(2, 0, 2, 2);
+        space.addView(barColour, new LayoutParams(-1, -1));
     }
 
     public GtkNotebook(Context context) {
         this(context, null);
     }
 
+    public void setBarColour(int colour) {
+        barColour.setBackgroundColor(colour);
+    }
+
     @Override
     public boolean onInterceptTouchEvent(MotionEvent motionEvent) {
-        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN)
-            startX = motionEvent.getX();
-        else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-            if (motionEvent.getX() - startX < -100 && getCurrentPage() + 1 < getPageCount())
-                setCurrentPage(getCurrentPage() + 1);
-            else if (motionEvent.getX() - startX > 100 && getCurrentPage() - 1 >= 0)
-                setCurrentPage(getCurrentPage() - 1);
-
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startX = motionEvent.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                if (motionEvent.getX() - startX < -100 && getCurrentPage() + 1 < getPageCount())
+                    setCurrentPage(getCurrentPage() + 1);
+                else if (motionEvent.getX() - startX > 100 && getCurrentPage() - 1 >= 0)
+                    setCurrentPage(getCurrentPage() - 1);
+                break;
         }
         return super.onInterceptTouchEvent(motionEvent);
     }
 
     public void appendPage(CharSequence title, View view) {
-        Space space = new Space(getContext());
-        Button tabTitle = new Button(getContext());
-        LinearLayout tabLayout = new LinearLayout(getContext());
-        tabLayout.addView(tabTitle, new LayoutParams(-1, 88));
-        tabLayout.addView(space, new LayoutParams(4, 100));
-        pageTitles.addView(tabLayout, new LinearLayout.LayoutParams(-1, 100, 1f));
+        Button tabLabel = new Button(getContext());
+        LinearLayout labelColour = new LinearLayout(getContext());
+        labelColour.setPadding(2, 0, 2, 2);
+        labelColour.addView(tabLabel, new LayoutParams(-1, -1));
+        pageTitles.addView(labelColour, new LayoutParams(304, 88));
         pageFrame.addView(view, new LayoutParams(-1, -1));
-        space.setBackgroundColor(Color.LTGRAY);
-        tabTitle.setText(title);
-        tabTitle.setBackgroundColor(Color.WHITE);
-        tabTitle.setGravity(Gravity.CENTER);
-        tabTitle.setOnClickListener(new OnClickListener() {
+        tabLabel.setText(title);
+        tabLabel.setBackgroundColor(Color.WHITE);
+        tabLabel.setGravity(Gravity.CENTER);
+        tabLabel.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                int page = pageTitles.indexOfChild((View) tabTitle.getParent());
+                int page = pageTitles.indexOfChild((View) tabLabel.getParent());
                 setCurrentPage(page);
             }
         });
@@ -77,6 +91,7 @@ public class GtkNotebook extends LinearLayout implements GtkWidget {
             }
         });
         view.setVisibility(INVISIBLE);
+        space.bringToFront();
     }
 
     public void setCurrentPage(int page) {
@@ -84,7 +99,9 @@ public class GtkNotebook extends LinearLayout implements GtkWidget {
         if (pageTitles.getChildAt(currentPage) != null) {
             pageFrame.getChildAt(previousPage).setVisibility(INVISIBLE);
             pageTitles.getChildAt(previousPage).setBackgroundColor(Color.WHITE);
-            pageTitles.getChildAt(currentPage).setBackgroundColor(Color.YELLOW);
+            pageTitles.getChildAt(previousPage).setElevation(0);
+            pageTitles.getChildAt(currentPage).setBackgroundColor(Color.parseColor("#bdb76b"));
+            pageTitles.getChildAt(currentPage).setElevation(4);
             pageFrame.getChildAt(currentPage).setVisibility(VISIBLE);
             previousPage = page;
         }
